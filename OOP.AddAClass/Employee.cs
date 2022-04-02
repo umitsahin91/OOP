937cc89a5aabcc3ed1e657201bf042a0a2d35777
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-02 15:12:53 +0300

    Expression-bodied propperty accessors

--- OOP.AddAClass/Employee.cs
+++ OOP.AddAClass/Employee.cs
@@ -2,7 +2,26 @@
 {
     class Employee
     {
-    
-     
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            set
+            {
+                _firstName = value;
+            }
+            get
+            {
+                return _firstName;
+            }
+        }
+
+        public string LastName 
+        { 
+            //Expression-bodied propperty accessors
+            get => _lastName; 
+            set => _lastName = value; 
+        }
     }
 }
--- OOP.AddAClass/Program.cs
+++ OOP.AddAClass/Program.cs
@@ -6,7 +6,11 @@ namespace OOP.AddAClass
     {
         static void Main(string[] args)
         {
-
+            Employee employee = new Employee();
+            employee.FirstName = "Ümit";
+            employee.LastName = "Şahin";
+            Console.WriteLine(employee.FirstName);
+            Console.WriteLine(employee.LastName);
 
         }
     }
