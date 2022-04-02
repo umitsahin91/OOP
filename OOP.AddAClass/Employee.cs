09c6498ad8eda89c0a907a028f4e2eb921e3e3b9
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-02 15:24:00 +0300

    Constructor

--- OOP.AddAClass/Employee.cs
+++ OOP.AddAClass/Employee.cs
@@ -17,14 +17,31 @@
             }
         }
 
-        public string LastName 
-        { 
+        public string LastName
+        {
             //Expression-bodied propperty accessors
-            get => _lastName; 
-            set => _lastName = value; 
+            get => _lastName;
+            set => _lastName = value;
         }
 
         //Auto-implemented propperty
         public int Age { get; set; }
+
+        //Constructor
+        public Employee()
+        {
+            System.Console.WriteLine("Yapılandırıcı metot....");
+        }
+
+        //Overloading
+        public Employee(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+       
+
     }
 }
--- OOP.AddAClass/Program.cs
+++ OOP.AddAClass/Program.cs
@@ -10,9 +10,16 @@ namespace OOP.AddAClass
             employee.FirstName = "Ümit";
             employee.LastName = "Şahin";
             employee.Age = 30;
-            Console.WriteLine(employee.Age);
             Console.WriteLine(employee.FirstName);
             Console.WriteLine(employee.LastName);
+            Console.WriteLine(employee.Age);
+
+            Console.WriteLine("------------");
+
+            Employee employee2 = new Employee("Mevlüt0", "Ateş", 35);
+            Console.WriteLine(employee2.FirstName);
+            Console.WriteLine(employee2.LastName);
+            Console.WriteLine(employee2.Age);
 
         }
     }
