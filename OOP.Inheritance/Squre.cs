71b11e914f17378f01877bc8d28bcdf926144963
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-02 20:49:20 +0300

    hiding method

--- OOP.Inheritance/Squre.cs
+++ OOP.Inheritance/Squre.cs
@@ -2,7 +2,10 @@
 {
     public class Square:Rectangle
     {
-
+        new public void Calculate()
+        {
+            System.Console.WriteLine("Calculated by Square...");
+        }
     }
 
 }
