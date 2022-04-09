7edc139492cbc9ed67feaf92470a8ef0c6868ee5
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-10 01:30:04 +0300

    IClonable

---
+++ OOP.Interface.IClonable_App/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interface.IClonable_App
+{
+    public class Book:ICloneable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        //a deep copy
+        //shalow copy
+        public object Clone()
+        {
+            //deep copy
+            //----------
+            //var book = new Book();
+            //book.Id = this.Id;
+            //book.Title = this.Title;
+            //book.Price = this.Price;
+            //return book;
+
+            return new Book()
+            {
+                Id = this.Id,
+                Title = this.Title,
+                Price = this.Price
+            };
+
+            //return this.MemberwiseClone();
+        }
+    }
+}
---
+++ OOP.Interface.IClonable_App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP.Interface.IClonable_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book()
+            {
+                Id = 1,
+                Title = "Suç ve Ceza",
+                Price = 25.5
+                
+            };
+
+            //var book2 = book1;
+            //book2.Title = "Sefiller";
+            //Console.WriteLine($"{book1.Id} {book1.Title} {book1.Price}");
+            //Console.WriteLine($"{book2.Id} {book2.Title} {book2.Price}");
+
+            var book3 = (Book)book1.Clone();
+            book3.Id = 2;
+            book3.Title = "Sefiller";
+            Console.WriteLine($"{book1.Id} {book1.Title} {book1.Price}");
+            Console.WriteLine($"{book3.Id} {book3.Title} {book3.Price}");
+        }
+    }
+}
