f8da6c9845b97d7a0076ad2fd91ab84f4971067c
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-10 02:36:35 +0300

    IEnumerable

---
+++ OOP.Interfaces.IEnumerableApp/Array.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace OOP.Interfaces.IEnumerableApp
+{
+    public class Array :IEnumerable
+    {
+        private int[] InnerList { get; set; }
+        private int index=0;
+        public Array(int size=16)
+        {
+            InnerList = new int[size];
+        }
+        public Array(params int[] initial)
+        {
+            InnerList = new int[initial.Length];
+            foreach (var item in initial)
+            {
+                InnerList[index] = item;
+                index++;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return InnerList.Take(index).GetEnumerator();
+        }
+    }
+}
---
+++ OOP.Interfaces.IEnumerableApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP.Interfaces.IEnumerableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CustomArr = new Array(1,2,3,5,6);
+            var arr = new int[] { 1, 3, 5, 7 };
+
+            //predefined
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(new string('-',25));
+            foreach (var item in CustomArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+    }
+}
