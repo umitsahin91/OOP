ae06a363e630d97aa8d85684324046b67bf8039f
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-10 02:05:26 +0300

    IComparable generic

---
+++ OOP.Interfaces.IComparable_T_App/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interfaces.IComparable_T_App
+{
+    public class Employee:IComparable<Employee>
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Salary { get; set; }
+
+        public int CompareTo(Employee other)
+        {
+            //if (this.Salary < other.Salary)
+            //    return -1;
+            //else if (this.Salary == other.Salary)
+            //    return 0;
+            //else
+            //    return 1;
+            //return this.FullName.CompareTo(other.FullName);
+            //return this.Id.CompareTo(other.Id);
+            //return this.Salary.CompareTo(other.Salary);
+            //Reverse Sort
+            if (this.Salary < other.Salary)
+                return 1;
+            else if (this.Salary == other.Salary)
+                return 0;
+            else
+                return -1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id,-2}| {FullName,-20} |{Salary,10}";
+        }
+    }
+}
---
+++ OOP.Interfaces.IComparable_T_App/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP.Interfaces.IComparable_T_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employess = new List<Employee>()
+            {
+                   new Employee()
+                   {
+                       Id=1,
+                       FullName="Ahmet Aslan",
+                       Salary=9500
+                   },
+                   new Employee()
+                   {
+                       Id=2,
+                       FullName="Aslan Yıldırım",
+                       Salary=3500
+                   },
+                   new Employee()
+                   {
+                       Id=3,
+                       FullName="Merve Dağ",
+                       Salary=12000
+                   },
+                   new Employee()
+                   {
+                       Id=4,
+                       FullName="Züleyha Çan",
+                       Salary=4250
+                   },
+                   new Employee()
+                   {
+                       Id=5,
+                       FullName="Çakır Büyüküstün",
+                       Salary=7500
+                   },
+                   new Employee()
+                   {
+                       Id=6,
+                       FullName="Birol Baş",
+                       Salary=13250
+                   }
+
+
+            };
+
+            employess.Sort();
+            
+            foreach (var employee in employess)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
