5e844b10130be77e6d0bc40a362112cb266f73d7
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-09 23:38:46 +0300

    IFormattable

---
+++ OOP.Interfaces.IFormattable/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOP.Interfaces.IFormattables
+{
+    public class Employee:IFormattable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{Title} {FirstName} {LastName}";
+
+        public override string ToString()
+        {
+            return this.ToString("F", CultureInfo.CurrentCulture);
+        }
+        public  string ToString(string format)
+        {
+            return this.ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+            {
+                format = "F";
+            }
+            if (formatProvider==null)
+            {
+                formatProvider = CultureInfo.CurrentCulture;
+            }
+            switch (format.ToUpper())
+            {
+                case "F":
+                    return $"{Id,-5} {Title,-20} {FirstName,-15} {LastName,-15}";
+                case "FL":
+                    return $"{FirstName} {LastName}";
+                case "LF":
+                    return $"{LastName} {FirstName}";
+                case "S":
+                    return $"{LastName} {FirstName[0]}.";
+                default:
+                    throw new FormatException(string.Format("The {0} is not supported.", format));
+            }
+
+        }
+    }
+}
---
+++ OOP.Interfaces.IFormattable/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP.Interfaces.IFormattables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Employee>()
+            {
+            new Employee()
+            {
+                Id = 1,
+                Title = "Prof. Dr.",
+                FirstName = "Ahmet",
+                LastName = "Dağ"
+            },
+            new Employee()
+            {
+                Id = 2,
+                Title = "Prof. Dr.",
+                FirstName = "Filiz",
+                LastName = "Çay"
+            },
+            new Employee()
+            {
+                Id = 3,
+                Title = "Doç. Dr.",
+                FirstName = "Merve",
+                LastName = "Canan"
+            },
+             new Employee()
+            {
+                Id = 4,
+                Title = "Dr. Öğr. Üyesi",
+                FirstName = "Nurettin",
+                LastName = "Han"
+            }
+        };
+
+            foreach (var employee in list)
+            {
+                Console.WriteLine(employee.ToString());
+                //Console.WriteLine(employee.ToString("LF"));
+                //Console.WriteLine(employee.ToString("FL"));
+                //Console.WriteLine(employee.ToString("S"));
+            }
+        }
+    }
+}
