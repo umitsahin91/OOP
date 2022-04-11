101fe959a30982087cccf779c64021353f0b0c39
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-11 16:47:47 +0300

    generic class definition

---
+++ OOP-Generics.ArrayApp/Array.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace OOP_Generics.ArrayApp
+{
+    public class Array<T>:IEnumerable
+    {
+        private T[] InnerList;
+        private int index;
+        public Array(int size=16)
+        {
+            InnerList = new T[size];
+            index = 0;
+        }
+        public Array(params T[] collection)
+        {
+            InnerList = new T[collection.Length];
+            index = 0;
+            foreach (var item in collection)
+            {
+                InnerList[index] = item;
+                index++;
+            }
+            
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return InnerList.Take(index).GetEnumerator();
+        }
+    }
+}
---
+++ OOP-Generics.ArrayApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP_Generics.ArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new Array<char>('a', 'b', 'c');
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
