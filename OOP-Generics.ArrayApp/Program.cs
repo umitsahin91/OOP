45dadc7dda3e2387de28dc59f72738a8efa92f5d
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-11 16:59:06 +0300

    generics and custom type

--- OOP-Generics.ArrayApp/Program.cs
+++ OOP-Generics.ArrayApp/Program.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP_Generics.ArrayApp
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            var list = new LinkedList<Book>();
+            list.AddFirst(new Book(1,"Sefiller",30));
+            list.AddFirst(new Book(2, "Suç ve Ceza", 40));
+            list.AddFirst(new Book(3, "Devlet", 50));
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void GenericClassDefinition()
         {
             var arr = new Array<char>('a', 'b', 'c');
 
