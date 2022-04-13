5e93255b2ea16423528cf445992445e4bf3dc001
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-13 03:24:33 +0300

    ADONET SqlConnection SqlCommand and insert into record

---
+++ OOP.ADONET/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace OOP.ADONET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //ADONET
+
+            //(C)reate
+
+            //Connection
+
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            var conn = new SqlConnection(connectionString);
+            //conn.ConnectionString = connectionString;
+            conn.Open();
+
+            //Command
+
+            var cmd = new SqlCommand("INSERT INTO Employees(FirstName, LastName) VALUES(@FirstName,@LastName)");
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("FirstName", "Zafer");
+            cmd.Parameters.AddWithValue("LastName", "Cömert");
+
+            var s = cmd.ExecuteNonQuery();
+            Console.WriteLine(s);
+
+            //(R)ead
+            //(U)pdate
+            //(D)elete
+
+
+
+        }
+    }
+}
